c6a0b0d78349eb4a4a907b7692393a16e029f7ab
Suriyakumar <surya.conversion@gmail.com>
2024-10-02 17:59:23 +0530

    Join Two Names

---
+++ script.js
@@ -0,0 +1,5 @@
+function fullName() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    document.getElementById("h1").innerHTML = fname + " " + lname;
+}
